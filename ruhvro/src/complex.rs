bf0e0701df90c2da4a3c207d4636d9d618379848
Tyler <tylerschauer@gmail.com>
2024-05-20 22:46:47 -0400

    Add map type

--- ruhvro/src/complex.rs
+++ ruhvro/src/complex.rs
@@ -1,12 +1,9 @@
 use crate::deserialize::{add_data_to_array, get_val_from_possible_union};
 use anyhow::{anyhow, Result};
 use apache_avro::types::Value;
-use arrow::array::{
-    make_builder, Array, ArrayBuilder, ArrayRef, BooleanBufferBuilder, BooleanBuilder, Datum,
-    GenericListArray, StructArray, UnionArray,
-};
+use arrow::array::{make_builder, Array, ArrayBuilder, ArrayRef, BooleanBufferBuilder, BooleanBuilder, Datum, GenericListArray, StructArray, UnionArray, MapArray, ArrayData, ListArray, AsArray};
 use arrow::buffer::{Buffer, NullBuffer, OffsetBuffer, ScalarBuffer};
-use arrow::datatypes::{DataType, Field, FieldRef, Fields, UnionFields};
+use arrow::datatypes::{DataType, Field, FieldRef, Fields, UnionFields, UnionMode};
 use std::any::Any;
 use std::collections::HashMap;
 use std::sync::Arc;
@@ -36,7 +33,9 @@ impl AvroToArrowBuilder {
                 Ok(AvroToArrowBuilder::Struct(Box::new(sc)))
             }
             DataType::Union(union_fields, union_type) => {
-                // let rewrapped = Arc::new()
+                let rewrapped = Arc::new(Field::new(field.name(), DataType::Union(union_fields.clone(), union_type.clone()), field.is_nullable()));
+                let uc = UnionContainer::try_new(rewrapped, capacity)?;
+                Ok(AvroToArrowBuilder::Union(Box::new(uc)))
             }
             // DataType::Map(_, _) => {}
             _ => Ok(AvroToArrowBuilder::Primitive(make_builder(
@@ -267,7 +266,7 @@ impl UnionContainer {
         })
     }
 
-    fn from_union_fields(union_fields: UnionFields) {}
+
     ///
     /// Adds value to Union. Union needs to track additional meta data
     fn add_val(&mut self, avro_val: &Value) -> Result<()> {
@@ -313,14 +312,57 @@ impl UnionContainer {
         Ok(Arc::new(arr))
     }
 }
-struct MapContainer {}
+
+struct MapContainer {
+    fields: FieldRef,
+    inner_list: ListContainer,
+}
+
+impl MapContainer {
+    fn try_new(field: FieldRef, capacity: usize) -> Result<Self> {
+        match field.data_type()  {
+            DataType::Map(fr, _ordered) => {
+                let wrapped  = Arc::new(Field::new("map_col", DataType::List(fr.clone()), field.is_nullable()));
+                let il = ListContainer::try_new(wrapped, capacity)?;
+                let mc = MapContainer {fields: field.clone(), inner_list: il};
+                Ok(mc)
+            }
+            _ => Err(anyhow!("Failed to create MapContainer"))
+        }
+    }
+    fn add_val(&mut self, avro_val: &Value) -> Result<()> {
+        let av = get_val_from_possible_union(avro_val, &self.fields);
+        match av {
+            Value::Map(hm) => {
+                let mut inside_vec = vec![];
+                for (k,v) in hm.iter() {
+                    let rewrapped = Value::Record(vec![("key".to_string(), Value::String(k.to_owned())), ("value".to_string(), v.to_owned())]);
+                    inside_vec.push(rewrapped)
+                }
+                let wrapped_list = Value::Array(inside_vec);
+                self.inner_list.add_val(&wrapped_list)?;
+            }
+            Value::Null => unimplemented!(),
+            _ => unreachable!()
+        }
+        Ok(())
+    }
+    fn build(mut self) -> Result<ArrayRef> {
+        let l_array = self.inner_list.build()?;
+        let list_arr = l_array.as_any().downcast_ref::<ListArray>().unwrap().to_owned();
+        let (a, b, c, d) = list_arr.into_parts();
+        let m = MapArray::try_new(a, b, c.as_any().downcast_ref::<StructArray>().unwrap().to_owned(), d, false)?;
+        Ok(Arc::new(m))
+    }
+}
 
 mod tests {
-    use crate::complex::{AvroToArrowBuilder, ListContainer, StructContainer, UnionContainer};
+    use std::collections::HashMap;
+    use crate::complex::{AvroToArrowBuilder, ListContainer, MapContainer, StructContainer, UnionContainer};
     use apache_avro::types::Value;
     use arrow::array::{
         Array, BooleanBufferBuilder, Int32Array, ListArray, StringArray, StringBuilder,
-        StructArray, UnionArray,
+        StructArray, UnionArray, RecordBatch
     };
     use arrow::datatypes::{DataType, Field, Fields, UnionFields, UnionMode};
     use std::sync::Arc;
@@ -493,4 +535,60 @@ mod tests {
             .to_string();
         assert_eq!("string time".to_string(), value);
     }
+    #[test]
+    fn test_map_array() {
+        let k_field = Field::new("key", DataType::Utf8, false);
+        let v_field = Field::new("value", DataType::Int32, false);
+        let struct_field = Arc::new(Field::new("struct_f", DataType::Struct(Fields::from(vec![k_field, v_field])), false));
+        let map_field = Field::new("map_f", DataType::Map(struct_field.clone(), false),false);
+        let mut mc = MapContainer::try_new(Arc::new(map_field), 5).unwrap();
+
+        let avro_val = Value::Map(HashMap::from_iter(vec![("my_key".into(), Value::Int(1)), ("second".into(), Value::Int(3))]));
+        let r = mc.add_val(&avro_val);
+        println!("{:?}", r);
+        let result = mc.build().unwrap();
+        println!("{:?}", result);
+
+
+
+    }
+    #[test]
+    fn test_nested_struct() {
+        let list_val_field = Arc::new(Field::new("in_list", DataType::Int32, false));
+        let list_field = Field::new("list_f", DataType::List(list_val_field), false);
+        let inside_nested_struct_int = Field::new("in_struct_int", DataType::Int32, false);
+        let inside_nested_struct_long = Field::new("in_struct_long", DataType::Int64, false);
+        let inside_struct_field = Field::new("inside_struct", DataType::Struct(Fields::from(vec![inside_nested_struct_int, inside_nested_struct_long])), false);
+        let list_of_structs = Field::new("list_of_structs", DataType::List(Arc::new(inside_struct_field)), false);
+        let outer_struct = Arc::new(Field::new("outer_struct", DataType::Struct(Fields::from(vec![list_field, list_of_structs])), false));
+
+        let mut sb = StructContainer::try_new(outer_struct, 1).unwrap();
+
+        let avro_list_val = Value::Int(1);
+        let avro_list_val2 = Value::Int(2);
+        let avro_list = Value::Array(vec![avro_list_val, avro_list_val2]);
+        let avro_inside_nested_struct_int = Value::Int(2);
+        let avro_inside_nested_struct_long =  Value::Long(3);
+        let avro_struct = Value::Record(vec![("in_struct_int".to_string(), avro_inside_nested_struct_int), ("in_struct_long".to_string(), avro_inside_nested_struct_long)]);
+        let avro_list_struct = Value::Array(vec![avro_struct]);
+        let avro_outer_struct = Value::Record(vec![("list_f".to_string(), avro_list), ("outer_struct".into(), avro_list_struct)]);
+
+        let _r = sb.add_val(&avro_outer_struct);
+        // println!("{:?}", _r);
+        let avro_list_val = Value::Int(2);
+        let avro_list = Value::Array(vec![avro_list_val]);
+        let avro_inside_nested_struct_int = Value::Int(3);
+        let avro_inside_nested_struct_long =  Value::Long(4);
+        let avro_struct = Value::Record(vec![("in_struct_int".to_string(), avro_inside_nested_struct_int), ("in_struct_long".to_string(), avro_inside_nested_struct_long)]);
+        let avro_list_struct = Value::Array(vec![avro_struct]);
+        let avro_outer_struct = Value::Record(vec![("list_f".to_string(), avro_list), ("outer_struct".into(), avro_list_struct)]);
+        sb.add_val(&avro_outer_struct);
+
+        let finished = sb.build();
+        let a: RecordBatch = finished.unwrap().as_any().downcast_ref::<StructArray>().unwrap().into();
+        assert_eq!(a.num_rows(), 2);
+        assert_eq!(a.columns().len(), 2);
+    }
 }
+// InvalidArgumentError("MapArray expected ArrayData with DataType::Map \
+// got List(Field { name: \"struct_f\", data_type: Struct([Field { name: \"key\", data_type: Utf8, nullable: false, dict_id: 0, dict_is_ordered: false, metadata: {} }, Field { name: \"value\", data_type: Int32, nullable: false, dict_id: 0, dict_is_ordered: false, metadata: {} }]), nullable: false, dict_id: 0, dict_is_ordered: false, metadata: {} })")
