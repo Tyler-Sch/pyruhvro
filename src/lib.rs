19dafb21ffeb4016cfa1d36e10398eedd62d8a6b
Tyler <tylerschauer@gmail.com>
2024-05-24 12:02:35 -0400

    Add feature to pass binary array in directly

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use arrow::array::{make_array, ArrayData, RecordBatch};
+use std::sync::Arc;
+use arrow::array::{make_array, ArrayData, RecordBatch, BinaryArray};
 use arrow::pyarrow::PyArrowType;
 use pyo3::prelude::*;
 use ruhvro::deserialize;
@@ -44,10 +45,29 @@ fn deserialize_arrow_threaded(
     Ok(i)
 }
 
+#[pyfunction]
+fn deserialize_array(list: Vec<&[u8]>, schema: &str) -> PyResult<PyArrowType<RecordBatch>> {
+   let input = Arc::new(BinaryArray::from_vec(list));
+    let parsed_schema = deserialize::parse_schema(schema).unwrap();
+    let r = deserialize::per_datum_deserialize_arrow(input, &parsed_schema);
+    Ok(PyArrowType(r))
+}
+
+#[pyfunction]
+fn deserialize_array_threaded(list: Vec<&[u8]>, schema: &str, num_chunks: usize) -> PyResult<Vec<PyArrowType<RecordBatch>>> {
+    let input = Arc::new(BinaryArray::from_vec(list));
+    let parsed_schema = deserialize::parse_schema(schema).unwrap();
+    let r = deserialize::per_datum_deserialize_arrow_multi(input, &parsed_schema, num_chunks);
+    let i = r.into_iter().map(|x| PyArrowType(x)).collect::<Vec<_>>();
+    Ok(i)
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyruhvro(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(deserialize_arrow, m)?)?;
     m.add_function(wrap_pyfunction!(deserialize_arrow_threaded, m)?)?;
+    m.add_function(wrap_pyfunction!(deserialize_array, m)?)?;
+    m.add_function(wrap_pyfunction!(deserialize_array_threaded, m)?)?;
     Ok(())
 }
