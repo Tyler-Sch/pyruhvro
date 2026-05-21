cfba199fe6787e6202376bca4d5dbaa709c59fdd
Tyler Schauer <tylerschauer@gmail.com>
2026-05-21 09:45:51 -0400

    Update dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -3,55 +3,63 @@
 //!
 use arrow::array::{Array, ArrayData, RecordBatch};
 use arrow::pyarrow::PyArrowType;
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
+use pyo3::pybacked::PyBackedBytes;
 use pyo3::types::PyList;
 use ruhvro::deserialize;
 use ruhvro::serialize;
 
-#[pyfunction]
-fn deserialize_array(list: &PyList, schema: &str) -> PyResult<PyArrowType<RecordBatch>> {
-    let parsed_schema = deserialize::parse_schema(schema).unwrap();
-    let borrow_list = list
-        .iter()
-        .map(|x| x.extract::<&[u8]>().unwrap())
-        .collect::<Vec<_>>();
-    let record_batch = deserialize::per_datum_deserialize(&borrow_list, &parsed_schema);
-    Ok(PyArrowType(record_batch))
+fn to_py_err<E: std::fmt::Display>(e: E) -> PyErr {
+    PyValueError::new_err(e.to_string())
 }
-#[pyfunction]
-fn deserialize_array_threaded(
-    list: &PyList,
-    schema: &str,
-    num_chunks: usize,
-) -> PyResult<Vec<PyArrowType<RecordBatch>>> {
-    let parsed_schema = deserialize::parse_schema(schema).unwrap();
-    let borrow_list = list
-        .iter()
-        .map(|x| x.extract::<&[u8]>().unwrap())
-        .collect::<Vec<_>>();
-    let record_batches =
-        deserialize::per_datum_deserialize_threaded(borrow_list, &parsed_schema, num_chunks);
-    let python_typed_batches = record_batches
-        .into_iter()
-        .map(|x| PyArrowType(x))
-        .collect::<Vec<_>>();
-    Ok(python_typed_batches)
+
+fn extract_bytes_list(list: &Bound<'_, PyList>) -> PyResult<Vec<PyBackedBytes>> {
+    list.iter()
+        .map(|x| x.extract::<PyBackedBytes>().map_err(PyErr::from))
+        .collect()
 }
 
 #[pyfunction]
-fn serialize_record_batch(data: PyArrowType<RecordBatch>, schema: &str, num_chunks: usize) -> PyResult<Vec<PyArrowType<ArrayData>>> {
-    let parsed_schema = deserialize::parse_schema(schema).unwrap();
-    let d = data.0;
-    let serialized = serialize::serialize_record_batch(d, &parsed_schema, num_chunks);
-    let python_typed_batches = serialized.into_iter()
-        .map(|x| PyArrowType(x.into_data())).collect::<Vec<_>>();
-    // Ok(PyArrowType(serialized.into_data()))
-    Ok(python_typed_batches)
+fn deserialize_array(list: &Bound<'_, PyList>, schema: &str) -> PyResult<PyArrowType<RecordBatch>> {
+    let parsed_schema = deserialize::parse_schema(schema).map_err(to_py_err)?;
+    let owned = extract_bytes_list(list)?;
+    let borrow_list: Vec<&[u8]> = owned.iter().map(|b| &b[..]).collect();
+    let record_batch = deserialize::per_datum_deserialize(&borrow_list, &parsed_schema);
+    Ok(PyArrowType(record_batch))
+}
+
+#[pyfunction]
+fn deserialize_array_threaded(
+    list: &Bound<'_, PyList>,
+    schema: &str,
+    num_chunks: usize,
+) -> PyResult<Vec<PyArrowType<RecordBatch>>> {
+    let parsed_schema = deserialize::parse_schema(schema).map_err(to_py_err)?;
+    let owned = extract_bytes_list(list)?;
+    let borrow_list: Vec<&[u8]> = owned.iter().map(|b| &b[..]).collect();
+    let record_batches =
+        deserialize::per_datum_deserialize_threaded(borrow_list, &parsed_schema, num_chunks);
+    Ok(record_batches.into_iter().map(PyArrowType).collect())
+}
+
+#[pyfunction]
+fn serialize_record_batch(
+    data: PyArrowType<RecordBatch>,
+    schema: &str,
+    num_chunks: usize,
+) -> PyResult<Vec<PyArrowType<ArrayData>>> {
+    let parsed_schema = deserialize::parse_schema(schema).map_err(to_py_err)?;
+    let serialized = serialize::serialize_record_batch(data.0, &parsed_schema, num_chunks);
+    Ok(serialized
+        .into_iter()
+        .map(|x| PyArrowType(x.into_data()))
+        .collect())
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn pyruhvro(_py: Python, m: &PyModule) -> PyResult<()> {
+fn pyruhvro(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(deserialize_array, m)?)?;
     m.add_function(wrap_pyfunction!(deserialize_array_threaded, m)?)?;
     m.add_function(wrap_pyfunction!(serialize_record_batch, m)?)?;
